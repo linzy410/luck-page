383098286158ff2cebe34ee97811e572ae964cdf
linzheyan <linzheyan@qnvip.com>
2016-09-27 15:51:53 +0800

    分为4个级别奖项，特等奖效果特殊，独立出来

--- js/luck.js
+++ js/luck.js
@@ -2,16 +2,37 @@ $(function() {
 	$('.jianju-nav li').click(function(){
 		$('.jianju-nav li').removeClass('active');
 		$(this).addClass('active');
+		$('.section').removeClass('active');
 		$('.section').fadeOut(1000);
 		var t = $(this).data('section');
-		setTimeout(function(){$('#'+t).fadeIn(1000);}, 1000);
+		setTimeout(function(){$('#'+t).fadeIn(1000);$('#'+t).addClass('active')}, 1000);
 	});
-	
-	
 	localStorage.setItem('ahm', ahm);
+	localStorage.setItem('spc', 0);
+	var spc = 0;
 	var timeArray = new Array();
 	var indexStr = ",";
 	$('.btn-rd').click(function() {
+		if (spc<2){
+			$(this).parent().parent().find('.table_rd>tbody>tr>td').each(function(i) {
+				var o = $(this);
+				var ti = setInterval(function() {preview(o);}, 50);
+				timeArray[i] = ti;
+			});
+			$(this).toggle();
+			$(this).next().toggle();
+			$(this).blur();
+		} else{
+			$('.nav li,.section.active').fadeOut(1000);
+			$('.section').removeClass('active');
+			$('#section_sp').addClass('active');
+			setTimeout(function(){
+				$('.nav li:last').fadeIn(1000);
+				$('#section_sp').fadeIn(1000);
+			}, 1000);
+		}
+	});
+	$('.btn-sp').click(function(){
 		$(this).parent().parent().find('.table_rd>tbody>tr>td').each(function(i) {
 			var o = $(this);
 			var ti = setInterval(function() {preview(o);}, 50);
@@ -19,13 +40,24 @@ $(function() {
 		});
 		$(this).toggle();
 		$(this).next().toggle();
+		$(this).blur();
 	});
 	$('.btn-rd-stop').click(function() {
+		$(this).hide();
+		if ($(this).data('jb')=='st'){
+			spc+=1;
+		}
+		if ($(this).data('jb')!='sp'){
+			setTimeout(function(){
+				if (spc >=2){
+					$('div.active .btn-rd').text('开启最终大奖');
+				}
+				$('div.active .btn-rd').fadeIn(2000);
+				}, 1000 * 0.5);
+		}
 		$.each(timeArray, function(i, v) {
 			clearInterval(v);
 		})
-		$(this).toggle();
-		$(this).prev().toggle();
 		var options = {
 			useEasing : true,
 			useGrouping : true,
@@ -47,7 +79,15 @@ $(function() {
 				});
 		hmStr = removeLuckhm(hmStr, indexStr.substring(1, indexStr.length-1));
 		localStorage.setItem('ahm', hmStr);
+		console.log(localStorage.getItem('ahm').split(',').length);
 	});
+	
+	function showSp() {
+		var height = $(window).height();
+		$("#section_sp").animate({top:'0px', height:height}, 2000, function(){$("#section_sp").removeClass('sec-sp-bg').addClass('bg');});
+		$("#section_sp").animate({width:'100%'}, 2000);
+	}
+	
 	function preview(obj) {
 		var i = getPreviewIndex();
 		var oldIndex = obj.data('index');
@@ -56,6 +96,7 @@ $(function() {
 		var array = localStorage.getItem('ahm').split(',');
 		obj.html(array[i]);
 	}
+	
 	function getPreviewIndex() {
 		var array = localStorage.getItem('ahm').split(',');
 		var i = Math.floor(Math.random() * array.length);
@@ -80,4 +121,14 @@ $(function() {
 		});
 		return source.substring(1, source.length-1);
 	}
+	
+	$(document).keydown(function(e){
+        var e = e || window.event;
+       if(e.keyCode==32) {
+    	   console.log(!$('div.active .btn-rd-stop').is(':hidden'));
+    	   if (!$('div.active .btn-rd-stop').is(':hidden')){
+    		   $('div.active .btn-rd-stop').trigger('click');
+    	   }
+       }
+    })
 });
